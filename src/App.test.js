a4d63d824087b507c6a4e464193a81700f05f42d
Abhishek Gupta <abhishek.gupta13@nagarro.com>
2022-06-15 18:10:10 +0530

    feat: adding test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Component renders with redux", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
