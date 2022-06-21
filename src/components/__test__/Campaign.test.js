69d77f6f0ec6357444f270170a89354ce00178ad
Abhishek Gupta <abhishek.gupta13@nagarro.com>
2022-06-21 16:23:49 +0530

    feat: adding tests

--- src/components/__test__/Campaign.test.js
+++ src/components/__test__/Campaign.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Campaign from "../Campaign";
-import { cleanup, render, screen } from "@testing-library/react";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 const mockData = [
@@ -54,20 +54,42 @@ describe("Test rendering of Campaign component", () => {
 
   test("it should render table data without crashing", () => {
     render(<Campaign data={mockData} allUsers={allUsers} />);
-    const label = screen.getByText("Bret");
-    expect(label).toBeInTheDocument();
+    const userName1 = screen.getByText("Bret");
+    expect(userName1).toBeInTheDocument();
+
+    const userName2 = screen.getByText("Antonette");
+    expect(userName2).toBeInTheDocument();
   });
 
-  test("it should provide correct type for date before focus", () => {
+  test("it should provide correct type for date before click", () => {
     render(<Campaign data={[]} allUsers={[]} />);
     const startDate = screen.getByPlaceholderText("Start Date");
     expect(startDate).toHaveAttribute("type", "text");
   });
 
-  test("it should provide correct type for date after focus", () => {
+  test("it should provide correct type for date after click", () => {
     render(<Campaign data={[]} allUsers={[]} />);
     const startDate = screen.getByPlaceholderText("Start Date");
     userEvent.type(startDate, "12/12/2022");
     expect(startDate).toHaveAttribute("type", "date");
+  });
+});
+
+describe("Testing components with different simulations", () => {
+  test("simulate reset button click", () => {
+    render(<Campaign data={[]} allUsers={[]} />);
+    const startDate = screen.getByPlaceholderText("Start Date");
+    const endDate = screen.getByPlaceholderText("End Date");
+    const resetButton = screen.getByText("Reset Date");
+    fireEvent.click(resetButton);
+    expect(startDate.value).toMatch("");
+    expect(endDate.value).toMatch("");
+  });
+
+  test("it should correctly filter based on name search", () => {
+    render(<Campaign data={mockData} allUsers={allUsers} />);
+    const searchName = screen.getByPlaceholderText("Search by name");
+    userEvent.type(searchName, "Jaxspan");
+    expect(screen.queryByText("Divavu")).not.toBeInTheDocument();
   });
 });
